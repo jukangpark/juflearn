f46c94b049be3e6b4d63801ec808a641036d362e
jukangpark <skyxxx9339@naver.com>
2022-10-18 10:54:19 +0900

    bcrypt 암호화 주석 처리

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -8,6 +9,12 @@ const userSchema = new mongoose.Schema({
   reviews: [{ type: mongoose.Schema.Types.ObjectId }],
   createdAt: { type: Date, required: true, default: Date.now },
 });
+
+// userSchema.pre("save", async function () {
+//   if (this.isModified("password")) {
+//     this.password = await bcrypt.hash(this.password, 5);
+//   }
+// }); // 해시함수 를 통해 비밀 번호 암호화
 
 const User = mongoose.model("User", userSchema);
 
