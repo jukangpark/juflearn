7f387beceae7c6fef7940b7fbe37dfaaf9c980f6
jukangpark <skyxxx9339@naver.com>
2022-11-04 16:47:55 +0900

    회원가입

--- client/src/pages/auth/Join.tsx
+++ client/src/pages/auth/Join.tsx
@@ -1,10 +1,16 @@
+import { useForm } from "react-hook-form";
 import Button from "../../components/auth/Button";
 
 const Join = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <div>
+      <h3>회원가입</h3>
       <form>
-        <input />
+        <input placeholder="id" />
+        <input placeholder="password" type="password" />
+        <input placeholder="비밀 번호 확인" type="password" />
         <Button>회원가입</Button>
       </form>
     </div>
