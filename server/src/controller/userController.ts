3a80d9ef7e9a627e8bcac8ede51483024a4e0f5f
jukangpark <skyxxx9339@naver.com>
2022-11-16 15:49:28 +0900

    join 컨트럴러

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -6,7 +6,15 @@ export const join = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   try {
-    await User.create({ email, password });
+    const isExist = await User.findOne({ email: email });
+
+    if (isExist) {
+      return res
+        .status(400)
+        .json({ message: "해당 이메일을 가진 유저가 존재합니다." });
+    } else {
+      await User.create({ email, password });
+    }
   } catch (err) {
     const error = err as Error;
     return errorHandler(error, req, res);
