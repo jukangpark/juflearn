8a4bdd2a7560cc7dfee4a18056ae0f055e8d078f
jukangpark <skyxxx9339@naver.com>
2023-10-08 18:41:51 +0900

    _app.tsx -> RecoilApolloWrapper 로 이름 변경 & RecoilRoot 추가

--- src/_app.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ApolloProvider } from "@apollo/client";
-import client from "../apollo-client";
-
-export default function MyApp({ children }: any) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-}
